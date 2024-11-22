f6f794eafb624e09e87d001cf4f46a2df39c8207
Mike Hemberger <jivedig@JiveDigs-Laptop.local>
2024-11-22 15:25:22 -0500

    build

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rsync -avrz --dry-run * promatchups:/home/bizbud22/domains/promatchups.com/public_html/wp-content/plugins/mai-asknews
-
+rsync -avrz --dry-run * promatchups:/home/bizbud22/domains/dev.promatchups.com/public_html/wp-content/plugins/mai-asknews
